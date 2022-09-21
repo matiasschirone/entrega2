578db3954f71e79518bdc08e3903d504a7f0419c
matiasschirone <matiasschirone@gmail.com>
2022-09-21 10:49:42 -0300

    correccion

--- contenedores/ContenedorFireBase.js
+++ contenedores/ContenedorFireBase.js
@@ -1,41 +1,80 @@
-import admin from 'firebase-admin';
-import config from '../config.js';
+const admin = require("firebase-admin");
+const serviceAccount = require("../utils/backend-ejemplo-5fafa-firebase-adminsdk-pj5ka-bba89c1f76.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(config.firebase)
+	credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore();
 
-class ContenedorFireBase {
-    constructor(collection) {
-        this.collection = db.collection(collection);
-    }
+class ContenedorFirebase {
+	constructor(coll) {
+		this.coll = coll;
+		this.connexion();
+		this.query = db.collection(coll);
+	}
 
-    async listar(id) {
-        try {
-            const doc = await this.collection.doc(id).get();
-            if (!doc.exists) {
-                throw new Error('No se encontró el documento');
-            }else{
-                const data = doc.data();
-                return {...data, id};
-            }
-        }catch(error){
-            throw new Error(error);
-        }
+	async connexion() {
+		console.log("Firestore: base de datos conectada");
+	}
 
-    }
-    async listarAll() {
-        try {
-            const result = [];
+	async save(obj) {
+		try {
+			let guardar = await this.query.add(obj);
+			return guardar.id;
+		} catch (error) {
+			console.log(`error al guardar: ${error}`);
+		} finally {
+		}
+	}
 
-        }catch(error){
-            throw new Error(error);
-        }
-    }
+	// traer producto por id
+	async getById(id) {
+		try {
+			let datos = await this.query.doc(id).get();
+			let newDatos = { ...datos.data(), id: datos.id };
+			return newDatos;
+		} catch (error) {
+			return `No se pudo traer producto ${id}. ${error}`;
+		} finally {
+		}
+	}
 
+	//traer todos los productos
+	async getAll() {
+		try {
+			let querySnapshot = await this.query.get();
+			let docs = querySnapshot.docs;
+			let newDatos = docs.map(doc => ({
+				...doc.data(),
+				id: doc.id
+			}));
+			return newDatos;
+		} catch (error) {
+			console.log(`error al listar: ${error}`);
+			return [];
+		} finally {
+		}
+	}
 
+	// eliminar producto por id
+	async deleteById(id) {
+		try {
+			let datos = await this.query.doc(id).delete();
+			return datos;
+		} catch (error) {
+			console.log(`error al eliminar: ${error}`);
+		} finally {
+		}
+	}
+
+	async updateById(obj) {
+		try {
+			await this.query.doc(obj.id).update(obj);
+			return obj.id;
+		} catch (error) {
+			console.log(`error al actualizar: ${error}`);
+		}
+	}
 }
 
-export default ContenedorFireBase;
+module.exports = ContenedorFirebase;
